26c07e7b52c479ca25e5af6279dfe3a118cb2aca
JacquesCorentin <c.jacques@rns.ecolescreatives.com>
2021-03-10 10:17:18 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -4,10 +4,28 @@
 #include "Player.h"
 
 void Player::getName(std::string_name){
-    std::cout<<"Vous vous nommez :"<< _name <<"."<<std:endl;
+    std::cout<<"Vous vous nommez :"<< _name <<"."<<std::endl;
     return
 }
 
-void Player::getScore(int _bestscore1)
-    std::cout<<"Votre meilleur score est de :"<< _bestscore1<<"."<< std:endl;
-void bestScore
+void Player::getScore(int _bestscore1){
+    std::cout<<"Votre meilleur score est de :"<< _bestscore1<<"."<< std::endl;
+    return
+}
+
+void Player::updateScore1(std::string _name){
+    std::cout<<"Entrer votre pseudo :"<<std::endl;
+    return
+}
+
+
+void Player::setscore(int_newscore1){
+    (newscore1)==0;
+    std::cout<<"Entrer votre nouveau score :";
+    cin>> _newscore1;
+    if((_newscore1)>=(_bestscore1))
+    
+    
+    return
+}
+
