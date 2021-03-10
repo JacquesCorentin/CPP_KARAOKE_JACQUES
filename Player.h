7240e54910e68e65997e93ec77a9d43044b3c080
JacquesCorentin <c.jacques@rns.ecolescreatives.com>
2021-03-10 10:17:22 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,20 +3,28 @@
 class Player{
     
     private:
-    std:string_name()
-    int _bestscore1()
-    int _bestscore2()
-    int _bestscore3()
+    std::string_name;
+    int _bestscore1;
+    int _bestscore2;
+    int _bestscore3;
+    int _bestscore4;
+    int _bestscore5;
 
+    int _newscore1;
+    int _newscore2;
+    int _newscore3;
+    int _newscore4;
+    int _newscore5;
 
+    int setscore1;
 
 
     public:
     void getName(std:string_name);
-    void getScore(int _bestscore);
-    void updateScore();
+    void getScore(int _bestscore1);
+    void updateScore1(int _bestscore1, int _newscore1);
 
 
-    Player();
+    Player(std::sting_name, int _bestscore);
 
 };
