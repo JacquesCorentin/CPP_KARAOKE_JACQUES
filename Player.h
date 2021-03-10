494cc8e506de8113c936c6f70efe8f9860e1aa80
JacquesCorentin <c.jacques@rns.ecolescreatives.com>
2021-03-10 09:41:37 +0100

    Update Player.h

---
+++ Player.h
@@ -0,0 +1,20 @@
+#include <string>
+
+class Player{
+    
+    private:
+    std:string_name()
+    int _bestscore()
+
+
+
+
+    public:
+    void getName(std:string_name);
+    void getScore(int _bestscore);
+    void updateScore();
+
+
+    Player();
+
+};
