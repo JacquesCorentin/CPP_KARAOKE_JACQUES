dff1fcf1a40e790b9ebec3ee4a7ff0216e1d0b01
JacquesCorentin <c.jacques@rns.ecolescreatives.com>
2021-03-10 11:48:32 +0100

    Update Karaoke.cpp

--- Karaoke.cpp
+++ Karaoke.cpp
@@ -5,4 +5,20 @@
 
 using namespace std;
 
-int score1;
+int main{
+
+    Player.getName ("Flore")
+
+    Player.getScore1
+    Player.getScore2
+    Player.getScore3
+    Player.getScore4
+    Player.getScore5
+    
+    Player.updateScore1
+    Player.updateScore2
+    Player.updateScore3
+    Player.updateScore4
+    Player.updateScore5
+    
+}
