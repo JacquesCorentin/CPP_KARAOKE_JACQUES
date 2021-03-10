811df3f72080296047609865b3eddeadbf744160
JacquesCorentin <c.jacques@rns.ecolescreatives.com>
2021-03-10 10:16:53 +0100

    Update Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "Player.h"
+
+void Player::getName(std::string_name){
+    std::cout<<"Vous vous nommez :"<< _name <<"."<<std:endl;
+    return
+}
+
+void Player::getScore(int _bestscore1)
+    std::cout<<"Votre meilleur score est de :"<< _bestscore1<<"."<< std:endl;
+void bestScore
